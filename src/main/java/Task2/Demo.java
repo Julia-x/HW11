46e720530cc32b4aceba1e8851e016669c3ec44e
Julia-x <55034307+Julia-x@users.noreply.github.com>
2024-03-01 23:58:28 +0200

    Demo.java

--- src/main/java/Task2/Demo.java
+++ src/main/java/Task2/Demo.java
@@ -9,10 +9,10 @@ import java.util.stream.Stream;
 
 public class Demo {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("1. John", "2. Bill", "3. Filip", "4. Ivan", "5. Stepan");
+        List<String> names = Arrays.asList("John", "Bill", "Filip", "Ivan", "Stepan");
         List<String> sortedUpperCaseName = names.stream()
                 .map(String::toUpperCase)
-                .sorted(Comparator.comparing(s -> s.split("\\. ")[1], Comparator.reverseOrder()))
+                .sorted(Comparator.reverseOrder())
                 .collect(Collectors.toList());
                 System.out.println(sortedUpperCaseName);
     }
