6a857aded86e707adf27f5a53e8becf0219b2db0
Julia-x <55034307+Julia-x@users.noreply.github.com>
2024-03-01 23:57:45 +0200

    Demo.java

--- src/main/java/Task1/Demo.java
+++ src/main/java/Task1/Demo.java
@@ -7,17 +7,14 @@ import java.util.stream.Stream;
 
 public class Demo {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("1.John", "2. Bill", "3. Filip", "4. Ivan", "5. Stepan");
-        Stream<String> nameStream = names.stream()
-                .filter(name -> {
-                    int index = Integer.parseInt(name.split("\\.")[0]);
-                    return index % 2 != 0;
-                })
+    List<String> names = Arrays.asList("John", "Bill", "Filip", "Ivan", "Stepan");
+        Stream<String> nameStream = IntStream.range(0, names.size())
+                .filter(index -> index % 2 == 0)
+                .mapToObj(index -> (index + 1) + ". " + names.get(index))  // додаємо індекс ім'я
                 .collect(Collectors.joining(", ")).lines();
-
 
         List<String> filteredNames = nameStream.collect(Collectors.toList());
 
-       System.out.println(filteredNames);
+        System.out.println(filteredNames);
     }
 }
